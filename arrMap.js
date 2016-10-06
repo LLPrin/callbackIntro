251e1f418d63095ff05d0ee529491dab047dd4e6
LLPrin <LLPrin@users.noreply.github.com>
2016-10-06 22:00:32 +0000

    created function code

---
+++ arrMap.js
@@ -0,0 +1,18 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+//inputNumsIndex and inputArr aren't needed. Func only uses inputNums
+var result = input.map(function (inputNums, inputNumsIndex, inputArr){
+  //Square x & y
+  // add results together
+  //Math square root
+  return Math.sqrt(inputNums.x * inputNums.x + inputNums.y * inputNums.y);
+
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
